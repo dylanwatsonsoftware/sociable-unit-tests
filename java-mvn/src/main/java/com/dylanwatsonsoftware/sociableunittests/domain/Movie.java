0de2f2049646248a5de4ea720dd6034e7c83114c
Dylan Watson <lotsabackscatter@gmail.com>
2021-10-29 23:27:27 +0800

    Create Movie.java

---
+++ java-mvn/src/main/java/com/dylanwatsonsoftware/sociableunittests/domain/Movie.java
@@ -0,0 +1,34 @@
+package com.dylanwatsonsoftware.sociableunittests;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+  public enum Type {
+    REGULAR, NEW_RELEASE, CHILDREN, UNKNOWN;
+  }
+ 
+  private String title;
+  Price price;
+
+private void setPriceCode( Movie.Type priceCode) {
+ switch (priceCode) {
+  case CHILDREN:
+price = new ChildrensPrice();
+  break;
+case NEW_RELEASE:
+price = new NewReleasePrice();
+break;
+case REGULAR: price = new RegularPrice();
+break;
+default:
+throw new IllegalArgumentException( "invalid price code");
+} }
+
+public double getCharge(int daysRented) { return price.getCharge(daysRented); }
+public int getPoints(int daysRented) { return price.getPoints(daysRented); }
+}
+
+public abstract class Price { abstract double getCharge(int daysRented); int getPoints(int daysRented) { return 1; } } public class ChildrensPrice extends Price { @Override double getCharge(int daysRented) { double amount = 1.5; if (daysRented > 3) amount += (daysRented -3) * 1.5; return amount; } }
+
+public class RegularPrice extends Price { @Override public double getCharge(int daysRented) { double amount = 2; if (daysRented > 2) amount += (daysRented -2) * 1.5; return amount; } } public class NewReleasePrice extends Price { @Override public double getCharge(int daysRented) { return daysRented * 3; } @Override int getPoints(int daysRented) { if (daysRented > 1) return 2; return 1; } }
