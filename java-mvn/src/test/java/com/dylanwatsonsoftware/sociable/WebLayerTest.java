db9747cb4f1d4c88ad1539c3db92af6325f4f20d
Dylan Watson <lotsabackscatter@gmail.com>
2021-11-07 09:49:35 +0800

    Update WebLayerTest.java

--- java-mvn/src/test/java/com/dylanwatsonsoftware/sociable/WebLayerTest.java
+++ java-mvn/src/test/java/com/dylanwatsonsoftware/sociable/WebLayerTest.java
@@ -19,7 +19,7 @@ public class WebLayerTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
             .andExpect(content().string(containsString("Hello, World")));
     }
 }
