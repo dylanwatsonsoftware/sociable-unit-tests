18a8ae537f3b3fcf93297bca6425afa508637508
Dylan Watson <lotsabackscatter@gmail.com>
2021-11-07 14:01:42 +0800

    Add brackets

--- java-mvn/src/test/java/com/dylanwatsonsoftware/sociable/api/SociableTest.java
+++ java-mvn/src/test/java/com/dylanwatsonsoftware/sociable/api/SociableTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class SociableTest {
 
     @Test
-    public void shouldBeFaster throws Exception {
+    public void shouldBeFaster() throws Exception {
         new RentalController().rental();
         new RentalController().greeting();
     }
