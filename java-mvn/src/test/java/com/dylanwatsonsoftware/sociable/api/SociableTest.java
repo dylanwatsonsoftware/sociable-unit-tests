94cc3735e79f91e0c7a1807de0b3d873e385c0a1
Dylan Watson <lotsabackscatter@gmail.com>
2021-11-07 13:57:09 +0800

    Create SociableTest.java

---
+++ java-mvn/src/test/java/com/dylanwatsonsoftware/sociable/api/SociableTest.java
@@ -0,0 +1,12 @@
+package com.dylanwatsonsoftware.sociable;
+
+import org.junit.jupiter.api.Test;
+
+public class SociableTest {
+
+    @Test
+    public void shouldBeFaster throws Exception {
+        new RentalController().rental();
+        new RentalController().greeting();
+    }
+}
